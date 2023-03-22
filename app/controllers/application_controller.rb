de83e0e42caa77499ffbd813b30eff61d5343ae8
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-22 11:45:28 +0300

    Completed assignment'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  # To use cookies hash in our controllers we include the above. This is because all our controllers inherit from the Application Controller adding this module generally means that all our controllers are able to work with cookies.
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  
+  # The method below helps us set the values on the session hash and the cookies hash and then serializing them in the resoinse so that we can view their values in the browser.
   def index
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
@@ -7,3 +7,4 @@ class SessionsController < ApplicationController
   end
 
 end
+# session and cookies hashes can both be stored in key value pairs of data. 
